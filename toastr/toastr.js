0345a6196f7cbe81460b14a10974e5c07cd4d867
Chou Tung <mymyopa@gmail.com>
2015-07-10 22:44:00 +0800

    研究了一下toastr 順便寫了README.md

--- toastr/toastr.js
+++ toastr/toastr.js
@@ -176,7 +176,8 @@
                         warning: 'toast-warning'
                     },
                     iconClass: 'toast-info',
-                    positionClass: 'toast-top-right',
+                    /************改變位置 不會 所以從這裡改XD************/
+                    positionClass: 'toast-bottom-full-width',
                     timeOut: 5000, // Set timeOut and extendedTimeOut to 0 to make it sticky
                     titleClass: 'toast-title',
                     messageClass: 'toast-message',
